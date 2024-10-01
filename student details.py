e1713e8b661b93d6caac1f23fce2cc0138c1fa94
sreeharii007 <sreeharipramod2028@cs.sjcetpalai.ac.in>
2024-10-01 15:40:51 +0530

    document header added

--- student details.py
+++ student details.py
@@ -1,3 +1,6 @@
+#name:Sreehari pramod
+#date:1-10-2024
+#to view name,cgpa and rollno
 name=input("Enter the name of the student:")
 roll_no=int(input("Enter the roll no:"))
 cgpa=float(input("Enter the CGPA:"))
