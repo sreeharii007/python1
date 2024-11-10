db006f5a7a6cecb47829850fae2e04bb5159323e
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-11-10 10:28:08 +0530

    Add files via upload

--- student details.py
+++ student details.py
@@ -1,7 +1,1 @@
-#name:Sreehari pramod
-#date:1-10-2024
-#to view name,cgpa and rollno
-name=input("Enter the name of the student:")
-roll_no=int(input("Enter the roll no:"))
-cgpa=float(input("Enter the CGPA:"))
-print("percentage:",cgpa*10,"%",sep='')
+print("Hello World")
